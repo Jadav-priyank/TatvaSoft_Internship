7e5734cf696abfc233c457a1c98b6d8a37167274
Jadav-Priyank <priyankjadav8@gmail.com>
2025-07-15 17:54:22 +0530

    commit

---
+++ UserApi/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserApi.Data;
+using UserApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public UsersController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        // GET api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
